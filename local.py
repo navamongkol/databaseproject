d49029cc4cf01eb4a7159c71a32ab5152f092833
navamongkol tongta <u6213387@microsoft.au.edu>
2021-03-04 21:40:46 +0700

    Test

--- local.py
+++ local.py
@@ -1,8 +1,9 @@
 import sqlite3
-from common import config
-from cv import backend
-
+# from common import config
+# from cv import backend
+LOCAL_DB = 'local.db'
 class LocalStorage:
+    
     def __init__(self):
         # self.connection = sqlite3.connect(config.LOCAL_DB)
         # self.cursor = self.connection.cursor()
@@ -15,25 +16,88 @@ class LocalStorage:
 
 
     def initializeDatabase(self):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
+        with sqlite3.connect(LOCAL_DB) as conn:
             cur = conn.cursor()
             cur.execute(
                 '''
-                CREATE TABLE Images
-                (
-                    id INTEGER,
-                    location_id INTEGER,
-                    body BLOB
+                CREATE TABLE Positions (
+                ID int NOT NULL,
+                PositionName varchar(100),
+                PRIMARY KEY (ID)
+                ) 
+                ''')
+
+            cur.execute(
+                '''
+                CREATE TABLE Addresses (
+                ID int NOT NULL,
+                Province varchar(100),
+                District varchar(100),
+                PRIMARY KEY (ID)
                 )
                 ''')
 
             cur.execute(
                 '''
-                CREATE TABLE UnknownImages
-                (
-                    id INTEGER,
-                    location_id INTEGER,
-                    body BLOB
+                CREATE TABLE Members (
+                ID int NOT NULL,
+                Name varchar(200),
+                AddressId INT,
+                PositionId INT,
+                PRIMARY KEY (ID),
+                FOREIGN KEY (PositionId) REFERENCES Positions(ID),
+                FOREIGN KEY (AddressId) REFERENCES Addresses(ID)
+                )
+                ''')
+            
+            cur.execute(
+                '''
+                CREATE TABLE Users (
+                ID int NOT NULL,
+                CitizenId varchar(13),
+                Name varchar(100),
+                BirthDate varchar(10),
+                PhoneNumber varchar(10),
+                AddressId INT,
+                PRIMARY KEY (ID),
+                FOREIGN KEY (AddressId) REFERENCES Addresses(ID)
+                )
+                ''')
+
+            cur.execute(
+                '''
+                CREATE TABLE Parties (
+                ID int NOT NULL,
+                MemberId INT,
+                PartyName varchar(100),
+                FavoriteCount INT,
+                UrlPicture varchar(100),
+                PRIMARY KEY (ID),
+                FOREIGN KEY (MemberId) REFERENCES Members(ID)
+                )
+                ''')
+
+            cur.execute(
+                '''
+                CREATE TABLE Logins (
+                ID int NOT NULL,
+                Role varchar(50),
+                UserId INT,
+                Password INT,
+                PRIMARY KEY (ID),
+                FOREIGN KEY (UserId) REFERENCES Users(ID),
+                )
+                ''')
+
+            cur.execute(
+                '''
+                CREATE TABLE Favorites (
+                ID int NOT NULL,
+                UserId INT,
+                PartyId INT,
+                PRIMARY KEY (ID),
+                FOREIGN KEY (UserId) REFERENCES Users(ID),
+                FOREIGN KEY (PartyId) REFERENCES Parties(ID)
                 )
                 ''')
 
@@ -41,7 +105,7 @@ class LocalStorage:
 
 
     def checkTableExist(self):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
+        with sqlite3.connect(LOCAL_DB) as conn:
             cur = conn.cursor()
             cur.execute(
                 '''
@@ -58,109 +122,109 @@ class LocalStorage:
                 return False
 
 
-    def get_features(self, top=None):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
-            cur = conn.cursor()
+    # def get_features(self, top=None):
+    #     with sqlite3.connect(LOCAL_DB) as conn:
+    #         cur = conn.cursor()
 
-            if top is None:
-                cur.execute(
-                    '''
-                    SELECT *
-                    FROM Images
-                    ''')
-            else:
-                cur.execute(
-                    '''
-                    SELECT *
-                    FROM Images
-                    LIMIT ?
-                    ''', [top])
+    #         if top is None:
+    #             cur.execute(
+    #                 '''
+    #                 SELECT *
+    #                 FROM Images
+    #                 ''')
+    #         else:
+    #             cur.execute(
+    #                 '''
+    #                 SELECT *
+    #                 FROM Images
+    #                 LIMIT ?
+    #                 ''', [top])
 
-            return [(x[0], x[1], backend.pickle_to_feature(x[2])) for x in cur.fetchall()]
+    #         return [(x[0], x[1], backend.pickle_to_feature(x[2])) for x in cur.fetchall()]
 
-    def get_feature_by_id(self, index):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
-            cur = conn.cursor()
-            cur.execute(
-                '''
-                SELECT *
-                FROM Images
-                WHERE id = ?
-                ''', [index])
+    # def get_feature_by_id(self, index):
+    #     with sqlite3.connect(LOCAL_DB) as conn:
+    #         cur = conn.cursor()
+    #         cur.execute(
+    #             '''
+    #             SELECT *
+    #             FROM Images
+    #             WHERE id = ?
+    #             ''', [index])
 
-            data = cur.fetchone()
+    #         data = cur.fetchone()
 
-            return (data[0], data[1], backend.pickle_to_feature(data[2]))
-
-
-    def get_feature_by_location_id(self, location_id):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
-            cur = conn.cursor()
-            cur.execute(
-                '''
-                SELECT *
-                FROM Images
-                WHERE location_id = ?
-                ''', [location_id])
-
-            return [(x[0], x[1], backend.pickle_to_feature(x[2])) for x in cur.fetchall()]
+    #         return (data[0], data[1], backend.pickle_to_feature(data[2]))
 
 
-    def add_feature(self, product_id, location_id, kp, desc):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
-            feature = backend.feature_to_pickle(kp, desc)
-            cur = conn.cursor()
+    # def get_feature_by_location_id(self, location_id):
+    #     with sqlite3.connect(LOCAL_DB) as conn:
+    #         cur = conn.cursor()
+    #         cur.execute(
+    #             '''
+    #             SELECT *
+    #             FROM Images
+    #             WHERE location_id = ?
+    #             ''', [location_id])
 
-            cur.execute(
-                '''
-                INSERT INTO Images
-                (id, location_id, body)
-                VALUES
-                (?, ?, ?)
-                ''', [product_id, location_id, feature])
-
-            conn.commit()
+    #         return [(x[0], x[1], backend.pickle_to_feature(x[2])) for x in cur.fetchall()]
 
 
-    def update_feature(self, index, kp, desc):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
-            feature = backend.feature_to_pickle(kp, desc)
-            cur = conn.cursor()
+    # def add_feature(self, product_id, location_id, kp, desc):
+    #     with sqlite3.connect(LOCAL_DB) as conn:
+    #         feature = backend.feature_to_pickle(kp, desc)
+    #         cur = conn.cursor()
 
-            cur.execute(
-                '''
-                UPDATE Images
-                SET body = ?
-                WHERE id = ?
-                ''', [feature, index])
+    #         cur.execute(
+    #             '''
+    #             INSERT INTO Images
+    #             (id, location_id, body)
+    #             VALUES
+    #             (?, ?, ?)
+    #             ''', [product_id, location_id, feature])
 
-            conn.commit()
-
-
-    def delete_features(self, ids):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
-            cur = conn.cursor()
-            idsQuery = tuple(ids)
-            cur.execute(
-                '''
-                DELETE FROM Images
-                WHERE id in ?
-                ''', [idsQuery])
-
-            conn.commit()
+    #         conn.commit()
 
 
-    def get_undetected_features_by_location_id(self, location_id):
-        with sqlite3.connect(config.LOCAL_DB) as conn:
-            cur = conn.cursor()
-            cur.execute(
-                '''
-                SELECT *
-                FROM UnknownImages
-                WHERE location_id = ?
-                ''', [location_id])
+    # def update_feature(self, index, kp, desc):
+    #     with sqlite3.connect(LOCAL_DB) as conn:
+    #         feature = backend.feature_to_pickle(kp, desc)
+    #         cur = conn.cursor()
 
-            return [(x[0], x[1], backend.pickle_to_feature(x[2])) for x in cur.fetchall()]
+    #         cur.execute(
+    #             '''
+    #             UPDATE Images
+    #             SET body = ?
+    #             WHERE id = ?
+    #             ''', [feature, index])
+
+    #         conn.commit()
+
+
+    # def delete_features(self, ids):
+    #     with sqlite3.connect(LOCAL_DB) as conn:
+    #         cur = conn.cursor()
+    #         idsQuery = tuple(ids)
+    #         cur.execute(
+    #             '''
+    #             DELETE FROM Images
+    #             WHERE id in ?
+    #             ''', [idsQuery])
+
+    #         conn.commit()
+
+
+    # def get_undetected_features_by_location_id(self, location_id):
+    #     with sqlite3.connect(LOCAL_DB) as conn:
+    #         cur = conn.cursor()
+    #         cur.execute(
+    #             '''
+    #             SELECT *
+    #             FROM UnknownImages
+    #             WHERE location_id = ?
+    #             ''', [location_id])
+
+    #         return [(x[0], x[1], backend.pickle_to_feature(x[2])) for x in cur.fetchall()]
 
 
 storage = LocalStorage()
